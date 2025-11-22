e473100e5d1baca6282ccfbde9f9c4992812f7ca
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-11-22 13:50:53 +0530

    added skeleton when loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import FeedPage from "./pages/FeedPage";
+import Footer from "./components/Footer";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -22,8 +24,26 @@ export default function App() {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen text-rose-600 font-bold text-xl">
-        Loading...
+      <div className="flex flex-col min-h-screen">
+        {/* Skeleton Navbar */}
+        <div className="h-16 border-b bg-white px-4 flex items-center justify-between">
+          <Skeleton className="h-8 w-40 rounded-md" />
+          <Skeleton className="h-10 w-24 rounded-full" />
+        </div>
+
+        {/* Skeleton Content */}
+        <div className="flex-1 max-w-5xl mx-auto w-full px-6 pt-12 lg:pt-24 flex flex-col lg:flex-row gap-12 items-center">
+          <div className="w-full lg:w-1/2 space-y-6">
+            <Skeleton className="h-16 w-3/4 rounded-lg" />
+            <Skeleton className="h-6 w-full rounded-md" />
+            <Skeleton className="h-6 w-2/3 rounded-md" />
+            <Skeleton className="h-14 w-40 rounded-full mt-4" />
+          </div>
+          <div className="hidden lg:block w-1/2">
+            <Skeleton className="h-80 w-80 rounded-2xl mx-auto" />
+          </div>
+          <Skeleton className="h-70 w-full rounded-md mt-10" />
+        </div>
       </div>
     );
   }
@@ -33,14 +53,13 @@ export default function App() {
       <Navbar user={user} />
       <Routes>
         {/* if user show feed */}
-        <Route path="/"
-          element={user ? <Navigate to="/feed" /> : <Hero />} />
+        <Route path="/" element={user ? <Navigate to="/feed" /> : <Hero />} />
         <Route
           path="/feed"
           element={user ? <FeedPage /> : <Navigate to="/" />}
         />
-
       </Routes>
+      <Footer />
     </Router>
   );
 }
